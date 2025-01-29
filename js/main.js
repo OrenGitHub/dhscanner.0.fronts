a00169b23fa93bdc6bb55f3cf0b49f2415ab6a49
OrenGitHub <tuna.is.good.for.you@gmail.com>
2025-01-29 20:20:41 +0200

    Add logging (Winston)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-const esprima = require('esprima')
-const express = require('express')
-const multer = require('multer')
+const multer = require('multer');
+const esprima = require('esprima');
+const express = require('express');
+const winston = require('winston');
 
 const app = express()
 const port = 3000
 
 const upload = multer();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp({ format: 'DD/MM/YYYY ( HH:mm:ss )' }),
+      winston.format.printf(({ timestamp, level, message }) => {
+        return `[${timestamp}] [${level.toUpperCase()}]: ${message}`;
+      })
+    ),
+    transports: [new winston.transports.Console()]
+});
 
 app.get('/healthcheck', (req, res) => {
   res.json({ 'healthy': true })
@@ -22,6 +34,7 @@ app.post('/to/esprima/js/ast', upload.single('source'), (req, res) => {
       ast = esprima.parseScript(sourceCode, { loc: true });
     }
     catch (error) {
+      logger.warn(`parse error: ${req.file.originalname}`)
       ast = { status: "FAIL" };
     }
   }
