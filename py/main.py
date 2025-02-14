7274d8d845bea288b4537ca54e007ebddae4970d
OrenGitHub <tuna.is.good.for.you@gmail.com>
2025-02-14 13:34:53 +0200

    remove quotes from docstrings - they cause problems later in dhscanner parser

--- py/main.py
+++ py/main.py
@@ -9,6 +9,13 @@ from typing import Final
 
 app = Flask(__name__)
 
+class ReplaceQuotes(ast.NodeTransformer):
+    def visit_Constant(self, node: ast.Constant) -> ast.Constant:
+        if isinstance(node.value, str):
+            node.value = node.value.replace('\'', '')
+            node.value = node.value.replace('"', '')
+        return node
+
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     return { 'healthy': True }
@@ -17,5 +24,6 @@ def healthcheck():
 def parse():
     fl = request.files['source']
     python_source_code = fl.read()
-    tree = ast.parse(python_source_code)
-    return ast.dump(tree, include_attributes=True)
+    original_tree = ast.parse(python_source_code)
+    normalized_tree = ReplaceQuotes().visit(original_tree)
+    return ast.dump(normalized_tree, include_attributes=True)
